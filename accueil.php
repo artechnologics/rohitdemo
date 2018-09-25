642161361f5395a41703d0adc534a53710995faa
artechnologics <artechnologics@gmail.com>
2018-09-25 18:53:22 +0530

    changes

---
+++ accueil.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="jumbotron">
+            <h2>CRM version 1.088 - 16 juillet 2018 </h2>
+        </div>
+    </div>
+</div>
